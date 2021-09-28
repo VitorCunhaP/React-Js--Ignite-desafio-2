e2633d9058a20d96cf171754947fc8a2af803eac
VitorCunhaP <69974844+VitorCunhaP@users.noreply.github.com>
2021-09-28 16:09:28 -0300

    concluded

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+
+import { Button } from "./components/Button";
+import { MovieCard } from "./components/MovieCard";
+
+import { SideBar } from "./components/SideBar";
+import { Content } from "./components/Content";
+
+import "./styles/global.scss";
+
+import "./styles/sidebar.scss";
+import "./styles/content.scss";
+
+export function App() {
+  const [selectedGenreId, setSelectedGenreId] = useState(1);
+
+  function handleClickButton(id: number) {
+    setSelectedGenreId(id);
+  }
+
+  return (
+    <div style={{ display: "flex", flexDirection: "row" }}>
+      <SideBar
+        selectedGenreId={selectedGenreId}
+        handleClickButton={handleClickButton}
+      />
+
+      <Content selectedGenreId={selectedGenreId} />
+    </div>
+  );
+}
